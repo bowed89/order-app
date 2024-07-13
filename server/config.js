301682d8a7aa29c468e14a1501a6da50a321d7d9
bowed89 <jesus.lopez.averanga@gmail.com>
2024-07-13 00:37:56 -0400

    second commi2t

--- server/config.js
+++ server/config.js
@@ -3,18 +3,18 @@ require('dotenv').config();
 
 // DIGITAL OCEAN VPS_PG
 try {
-    /* const con = new Pool({
-        connectionString: PG_CONNECTION_STRING
-    }); */
-
     const con = new Pool({
-        user: process.env.USER,
-        host: process.env.HOST,
-        database: process.env.DATABASE,
-        password: process.env.PASSWORD, // Asegúrate de que esta línea sea una cadena de texto
-        port: 5432, // Puerto por defecto de PostgreSQL
+        connectionString: process.env.PG_CONNECTION_STRING
     });
 
+    /*  const con = new Pool({
+         user: process.env.USER,
+         host: process.env.HOST,
+         database: process.env.DATABASE,
+         password: process.env.PASSWORD, // Asegúrate de que esta línea sea una cadena de texto
+         port: 5432, // Puerto por defecto de PostgreSQL
+     });
+  */
     con.connect();
     module.exports = con;
 
