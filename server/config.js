d0b61fc94e5c8329715402bfbcebf970d29e0a40
bowed89 <jesus.lopez.averanga@gmail.com>
2024-07-12 21:28:03 -0400

    second commi2t

--- server/config.js
+++ server/config.js
@@ -1,18 +1,17 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const { PG_CONNECTION_STRING, USER, HOST, DATABASE, PASSWORD, PORT } = process.env;
 // DIGITAL OCEAN VPS_PG
 try {
     /* const con = new Pool({
         connectionString: PG_CONNECTION_STRING
     }); */
     const con = new Pool({
-        user: USER,
-        host: HOST,
-        database: DATABASE,
-        password: "root", // Asegúrate de que esta línea sea una cadena de texto
-        port: PORT, // Puerto por defecto de PostgreSQL
+        user: process.env.USER,
+        host: process.env.HOST,
+        database: process.env.DATABASE,
+        password: process.env.PASSWORD.trim(), // Asegúrate de que esta línea sea una cadena de texto
+        port: process.env.PORT, // Puerto por defecto de PostgreSQL
     });
 
     con.connect();
